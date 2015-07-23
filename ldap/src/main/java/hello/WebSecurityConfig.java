311965ffdcdde8755ef971aca77f0716d3912b12
Faresis <nikignatiev@gmail.com>
2015-07-23 21:01:41 +0300

    spring ldap authentication + active directory support

--- ldap/src/main/java/hello/WebSecurityConfig.java
+++ ldap/src/main/java/hello/WebSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.authentication.configurers
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
+import org.springframework.beans.factory.annotation.*;
 
 @Configuration
 @EnableWebMvcSecurity
@@ -23,12 +25,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Configuration
     protected static class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
         @Override
-        public void init(AuthenticationManagerBuilder auth) throws Exception{
-            auth
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+            if (LDAP.equals(authType)) {
+                auth
                 .ldapAuthentication()
                 .userDnPatterns("uid={0},ou=people")
                 .groupSearchBase("ou=groups")
                 .contextSource().ldif("classpath:test-server.ldif");
+            } else if (AD.equals(authType)) {
+                ActiveDirectoryLdapAuthenticationProvider prov = new ActiveDirectoryLdapAuthenticationProvider(authDomain, authServer);
+                auth.authenticationProvider(prov);
+            }
         }
+
+        @Value("${authentication.type}")
+        private String authType;
+
+        @Value("${authentication.server}")
+        private String authServer;
+
+        @Value("${authentication.domain}")
+        private String authDomain;
+
+        private final String LDAP = "LDAP";
+        private final String AD = "AD";
     }
 }
